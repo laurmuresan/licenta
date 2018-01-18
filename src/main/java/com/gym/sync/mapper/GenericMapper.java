5bdaa49d7eb3da71dfbfb6441013d30d2928e550
Pop Iosif Cristian <iosif.pop@mhp.com>
2018-01-18 07:55:53 +0200

    added one mapper and changed the pom.xml

---
+++ src/main/java/com/gym/sync/mapper/GenericMapper.java
@@ -0,0 +1,25 @@
+package com.gym.sync.mapper;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+public abstract class GenericMapper<M, E extends Object> {
+
+    public abstract M toInternal(E epo);
+
+    public abstract E toExternal(M model);
+
+    public List<M> toInternals(Collection<E> epoList) {
+        List<M> modelList = new ArrayList<>();
+        epoList.stream().forEach(E -> modelList.add(toInternal(E)));
+        return modelList;
+    }
+
+    public List<E> toExternals(Collection<M> modelList) {
+        List<E> epoList = new LinkedList<>();
+        modelList.stream().forEach(M -> epoList.add(toExternal(M)));
+        return epoList;
+    }
+}
---
+++ src/main/java/com/gym/sync/mapper/LocationEpoMapper.java
@@ -0,0 +1,17 @@
+package com.gym.sync.mapper;
+
+import com.gym.sync.entity.utility.Location;
+import com.gym.sync.epo.LocationEpo;
+
+public class LocationEpoMapper extends GenericMapper<Location, LocationEpo> {
+
+    @Override
+    public Location toInternal(LocationEpo epo) {
+        return new Location(epo.getId(), epo.getAddress(), epo.getLongitude(), epo.getLatitude());
+    }
+
+    @Override
+    public LocationEpo toExternal(Location model) {
+        return new LocationEpo(model.getId(), model.getAddress(), model.getLongitude(), model.getLatitude());
+    }
+}
