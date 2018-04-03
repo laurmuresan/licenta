91aaa7dc57506f240212515553dcefc449123035
Pop Iosif Cristian <iosif.pop@mhp.com>
2018-04-03 13:45:07 +0300

    Changes on db xml and added properties

---
+++ src/main/java/com/gym/sync/GymSyncApplication.java
@@ -0,0 +1,14 @@
+package com.gym.sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author laurmuresan
+ */
+@SpringBootApplication
+public class GymSyncApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(GymSyncApplication.class, args);
+        }
+}
